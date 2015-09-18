57fa54b9c6399905206a624cc6b0db2301fd50ae
baiyidjp <271524878@qq.com>
2015-09-18 14:32:06 +0800

    Initial Commit

---
+++ recordToolDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  recordToolDemo
+//
+//  Created by I Smile going on 15/9/18.
+//  Copyright (c) 2015年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ recordToolDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  recordToolDemo
+//
+//  Created by I Smile going on 15/9/18.
+//  Copyright (c) 2015年 dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
